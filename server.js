1c6359cd7d4a4a529a3c2f38483a783e4e2e3309
FBetulDemir <fatmabetuld@gmail.com>
2024-10-15 12:02:47 +0200

    Added route for User Data

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ app.get('/', (req, res) => {
     res.send('Welcome to our API!');
 });
 
+app.get('/users', (req, res) => {
+    const users = [
+        { id: 1, name: 'John Doe' },
+        { id: 2, name: 'Jane Smith' },
+    ];
+    res.json(users);
+});
+
+app.use(express.json());
+
+app.post('users', (req, res) => {
+    const newUser = req.body;
+    console.log(newUser);
+    res.json({ message: 'User added Successfully!', user: newUser });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
