67ab30e288851f96fc3ef6197cdb8f7a604adf01
FBetulDemir <fatmabetuld@gmail.com>
2024-10-16 07:49:29 +0200

    Added get route /greet and post route /submit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Welcome to our API!');
 });
@@ -16,13 +18,22 @@ app.get('/users', (req, res) => {
     res.json(users);
 });
 
-app.use(express.json());
-
-app.post('users', (req, res) => {
+app.post('/users', (req, res) => {
     const newUser = req.body;
     console.log(newUser);
     res.json({ message: 'User added Successfully!', user: newUser });
 });
+
+app.get('/greet', (req, res) => {
+    res.send('Hello, user!');
+});
+
+app.post('/submit', (req, res) => {
+    const { name, age } = req.body;
+    res.send(`Hello, ${name}! You are ${age} years old.`);
+});
+
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
